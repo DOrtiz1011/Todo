1b1dc0f4835015e1f805ee6a5fad72e44b872bb5
Daniel Ortiz <DOrtiz1011@gmail.com>
2025-12-24 18:36:51 -0600

    Create TodoTaskController.cs

---
+++ Controllers/TodoTaskController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Models;
+using Todo.Repository;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoTaskController : Controller
+    {
+        private readonly ITodoTaskRepository _todoTaskRepository;
+
+        public TodoTaskController(ITodoTaskRepository todoTaskRepository)
+        {
+            _todoTaskRepository = todoTaskRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var tasks = await _todoTaskRepository.GetAllAsync();
+
+            return Ok(tasks);
+        }
+        
+        // GET: api/tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoTask>> GetById(int id)
+        {
+            var task = await _todoTaskRepository.GetByIdAsync(id);
+
+            if (task == null)
+            {
+                return NotFound(new { Message = $"Task with ID {id} not found." });
+            }
+
+            return Ok(task);
+        }
+    }
+}
